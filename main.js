50efe85c451fb5cc24131636590f283d78b529dd
RamyakJain <73877400+RamyakJain@users.noreply.github.com>
2020-11-13 11:52:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,44 @@ function take_snapshot(){
 });
 }
 console.log('ml5.version', ml5.version);
-classifier = ml5.imageClassifier('model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dkvzcic86/model.json',modelLoaded);
 function modelLoaded(){
     console.log('Model Loaded!');
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+   if (error){
+       console.error(error);
+   }
+   else{
+       console.log(results);
+       document.getElementById("result_emotion_name").innerHTML = results[0].label;
+       document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+   }
+   if (results[0].label == "happy"){
+       document.getElementById("update_emoji").innerHTML = "&#128522;";
+   }
+   if (results[0].label == "sad"){
+    document.getElementById("update_emoji").innerHTML = "&#128532;";
+
+}
+if (results[0].label == "angry"){
+    document.getElementById("update_emoji").innerHTML = "&#128548;";
+
+}
+if (results[1].label == "happy"){
+    document.getElementById("update_emoji2").innerHTML = "&#128522;";
+
+}
+if (results[1].label == "sad"){
+ document.getElementById("update_emoji2").innerHTML = "&#128532;";
+
+}
+if (results[1].label == "angry"){
+ document.getElementById("update_emoji2").innerHTML = "&#128548;";
+
+}
 }
